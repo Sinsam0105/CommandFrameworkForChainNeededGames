cf7f1173ce7b8aae5d62e925ba4bfea093d0b754
Sinsam <sejongjsw0105@gmail.com>
2026-06-03 13:39:25 +0900

    Remove PreviewAware dependency on preview scope

--- PreviewSnapshot.cs
+++ PreviewSnapshot.cs
@@ -1,28 +1,10 @@
-using System.Collections.Generic;
-
 namespace Sinsam.CommandFramework
 {
-    public sealed class PreviewSnapshot
-    {
-        private readonly IDictionary<object, object> _registry = DeepCloneHelper.NewRegistry();
-
-        public T GetClone<T>(T real) where T : class
-        {
-            if (real == null)
-            {
-                return null;
-            }
-
-            return DeepCloneHelper.AutoClone(real, markPreview: true, _registry);
-        }
-    }
-
     public static class PreviewAware
     {
         public static T Data<T>(T real) where T : class
         {
-            var snapshot = CommandPreviewScope.Snapshot;
-            return CommandPreviewScope.IsActive && snapshot != null ? snapshot.GetClone(real) : real;
+            return real;
         }
     }
 }
