49d6b3fde979dd3bafe684aba1d1241ee8e34743
Sinsam <sejongjsw0105@gmail.com>
2026-06-03 14:53:33 +0900

    Expand deep clone collection support

--- DeepCloneHelper.cs
+++ DeepCloneHelper.cs
@@ -17,11 +17,16 @@ namespace Sinsam.CommandFramework
     ///   - delegate                                   : 참조 유지
     ///   - [CloneReference] 필드                       : 참조 유지
     ///   - [CloneIgnore]  필드                         : 건너뜀(기본값)
-    ///   - IList&lt;T&gt;                              : 원소별 복사
+    ///   - [SelfClone]   필드                         : IDeepCloneable.DeepClone() 직접 호출
+    ///   - IList<T>                                   : 원소별 복사
+    ///   - IDictionary<TKey,TValue>                   : key/value별 복사
+    ///   - ISet<T> / HashSet<T>                       : 원소별 복사
     ///   - 그 외 class                                 : 생성자 우회 후 필드 재귀 복사
     ///
     /// 순환 참조와 공유 참조는 visited 맵으로 보존한다.
     /// markPreview=true면 복제된 모든 IPreviewable의 IsPreview를 true로 설정한다.
+    /// 일반 class 복제는 FormatterServices.GetUninitializedObject로 생성자를 우회한다.
+    /// Unity IL2CPP/AOT 환경에서는 이 경로의 호환성을 프로젝트 단위로 검증해야 한다.
     /// </summary>
     public static class DeepCloneHelper
     {
@@ -67,24 +72,16 @@ namespace Sinsam.CommandFramework
                 return existing;
 
             if (type.IsArray)
-            {
-                var srcArray = (Array)source;
-                var clonedArray = Array.CreateInstance(type.GetElementType(), srcArray.Length);
-                visited[source] = clonedArray;
-                for (int i = 0; i < srcArray.Length; i++)
-                    clonedArray.SetValue(CloneObject(srcArray.GetValue(i), visited, markPreview), i);
-                return clonedArray;
-            }
+                return CloneArray((Array)source, visited, markPreview);
+
+            if (source is IDictionary dictionary && type.IsGenericType)
+                return CloneDictionary(dictionary, type, visited, markPreview);
+
+            if (IsSetType(type))
+                return CloneSet((IEnumerable)source, type, visited, markPreview);
 
             if (source is IList list && type.IsGenericType)
-            {
-                var clonedList = (IList)Activator.CreateInstance(type);
-                visited[source] = clonedList;
-                foreach (var item in list)
-                    clonedList.Add(CloneObject(item, visited, markPreview));
-                MarkPreview(clonedList, markPreview);
-                return clonedList;
-            }
+                return CloneList(list, type, visited, markPreview);
 
             object clone = FormatterServices.GetUninitializedObject(type);
             visited[source] = clone;
@@ -93,11 +90,89 @@ namespace Sinsam.CommandFramework
             return clone;
         }
 
+        private static Array CloneArray(Array source, IDictionary<object, object> visited, bool markPreview)
+        {
+            var clonedArray = Array.CreateInstance(source.GetType().GetElementType(), source.Length);
+            visited[source] = clonedArray;
+            for (int i = 0; i < source.Length; i++)
+                clonedArray.SetValue(CloneObject(source.GetValue(i), visited, markPreview), i);
+            return clonedArray;
+        }
+
+        private static object CloneList(IList source, Type type, IDictionary<object, object> visited, bool markPreview)
+        {
+            var clone = (IList)CreateCollectionInstance(type);
+            visited[source] = clone;
+            foreach (var item in source)
+                clone.Add(CloneObject(item, visited, markPreview));
+            MarkPreview(clone, markPreview);
+            return clone;
+        }
+
+        private static object CloneDictionary(IDictionary source, Type type, IDictionary<object, object> visited, bool markPreview)
+        {
+            var clone = (IDictionary)CreateCollectionInstance(type);
+            visited[source] = clone;
+            foreach (DictionaryEntry entry in source)
+            {
+                object keyClone = CloneObject(entry.Key, visited, markPreview);
+                object valueClone = CloneObject(entry.Value, visited, markPreview);
+                clone.Add(keyClone, valueClone);
+            }
+            MarkPreview(clone, markPreview);
+            return clone;
+        }
+
+        private static object CloneSet(IEnumerable source, Type type, IDictionary<object, object> visited, bool markPreview)
+        {
+            var clone = CreateCollectionInstance(type);
+            visited[source] = clone;
+
+            MethodInfo addMethod = type.GetMethod("Add", BindingFlags.Instance | BindingFlags.Public);
+            if (addMethod == null)
+                throw new InvalidOperationException($"Set type '{type.FullName}' does not expose a public Add method.");
+
+            foreach (var item in source)
+                addMethod.Invoke(clone, new[] { CloneObject(item, visited, markPreview) });
+
+            MarkPreview(clone, markPreview);
+            return clone;
+        }
+
+        private static object CreateCollectionInstance(Type type)
+        {
+            if (!type.IsInterface && !type.IsAbstract)
+                return Activator.CreateInstance(type);
+
+            if (IsGenericDictionaryType(type))
+            {
+                Type[] args = type.GetGenericArguments();
+                return Activator.CreateInstance(typeof(Dictionary<,>).MakeGenericType(args));
+            }
+
+            if (IsGenericSetType(type))
+            {
+                Type[] args = type.GetGenericArguments();
+                return Activator.CreateInstance(typeof(HashSet<>).MakeGenericType(args));
+            }
+
+            if (IsGenericListType(type))
+            {
+                Type[] args = type.GetGenericArguments();
+                return Activator.CreateInstance(typeof(List<>).MakeGenericType(args));
+            }
+
+            throw new InvalidOperationException($"Collection type '{type.FullName}' cannot be cloned automatically.");
+        }
+
         private static object CloneStruct(object source, Type type, IDictionary<object, object> visited, bool markPreview)
         {
             object boxed = source;
             foreach (var field in type.GetFields(Flags))
             {
+                if (field.IsDefined(typeof(CloneIgnoreAttribute), true))
+                    continue;
+
                 Type ft = field.FieldType;
                 if (ft.IsPrimitive || ft.IsEnum || ft == typeof(string) || ft == typeof(decimal))
                     continue;
@@ -105,11 +180,12 @@ namespace Sinsam.CommandFramework
                 object value = field.GetValue(boxed);
                 if (value == null)
                     continue;
+
                 if (field.IsDefined(typeof(CloneReferenceAttribute), true) ||
                     value is UnityEngine.Object || value is Delegate)
                     continue;
 
-                field.SetValue(boxed, CloneObject(value, visited, markPreview));
+                field.SetValue(boxed, CloneFieldValue(field, value, visited, markPreview));
             }
             return boxed;
         }
@@ -137,15 +213,84 @@ namespace Sinsam.CommandFramework
                         continue;
                     }
 
-                    field.SetValue(clone, CloneObject(value, visited, markPreview));
+                    field.SetValue(clone, CloneFieldValue(field, value, visited, markPreview));
                 }
             }
+        }
+
+        private static object CloneFieldValue(FieldInfo field, object value, IDictionary<object, object> visited, bool markPreview)
+        {
+            if (field.IsDefined(typeof(SelfCloneAttribute), true))
+                return CloneBySelfClone(value, visited, markPreview);
+
+            return CloneObject(value, visited, markPreview);
+        }
+
+        private static object CloneBySelfClone(object source, IDictionary<object, object> visited, bool markPreview)
+        {
+            if (source == null)
+                return null;
+
+            if (visited.TryGetValue(source, out var existing))
+                return existing;
+
+            if (!(source is IDeepCloneable cloneable))
+                throw new InvalidOperationException(
+                    $"[SelfClone] field value '{source.GetType().FullName}' must implement IDeepCloneable.");
+
+            object clone = cloneable.DeepClone();
+            if (clone == null)
+                return null;
+
+            visited[source] = clone;
+            MarkPreview(clone, markPreview);
+            return clone;
         }
 
         private static void MarkPreview(object instance, bool markPreview)
         {
             if (markPreview && instance is IPreviewable previewable)
                 previewable.IsPreview = true;
+        }
+
+        private static bool IsSetType(Type type)
+        {
+            return IsGenericSetType(type) || ImplementsGenericDefinition(type, typeof(ISet<>));
+        }
+
+        private static bool IsGenericDictionaryType(Type type)
+        {
+            return type.IsGenericType &&
+                   (type.GetGenericTypeDefinition() == typeof(IDictionary<,>) ||
+                    type.GetGenericTypeDefinition() == typeof(Dictionary<,>));
+        }
+
+        private static bool IsGenericSetType(Type type)
+        {
+            return type.IsGenericType &&
+                   (type.GetGenericTypeDefinition() == typeof(ISet<>) ||
+                    type.GetGenericTypeDefinition() == typeof(HashSet<>));
+        }
+
+        private static bool IsGenericListType(Type type)
+        {
+            return type.IsGenericType &&
+                   (type.GetGenericTypeDefinition() == typeof(IList<>) ||
+                    type.GetGenericTypeDefinition() == typeof(List<>));
+        }
+
+        private static bool ImplementsGenericDefinition(Type type, Type genericDefinition)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == genericDefinition)
+                return true;
+
+            foreach (var interfaceType in type.GetInterfaces())
+            {
+                if (interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == genericDefinition)
+                    return true;
+            }
+
+            return false;
         }
 
         private sealed class ReferenceEqualityComparer : IEqualityComparer<object>
