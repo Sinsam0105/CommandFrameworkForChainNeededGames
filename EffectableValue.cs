11cf653ae9e171fd1e7fded2ab220c8f6a23ba7c
Sinsam <sejongjsw0105@gmail.com>
2026-06-20 20:33:43 +0900

    Adopt modifier-based effectable values

--- EffectableValue.cs
+++ EffectableValue.cs
@@ -1,130 +1,147 @@
 using System;
-using UnityEngine;
+using System.Collections.Generic;
 
-/// <summary>
-/// 커맨드 파이프라인에서 EditEvent로 보정 가능한 값의 인터페이스.
-/// Reset: 임시 보정값 초기화 (커맨드 1회 실행 후)
-/// Set:   임시 보정값을 영구 반영
-/// </summary>
-public interface IEffectableValue
+namespace Sinsam.CommandFramework
 {
-    void Reset();
-    void Set();
+    public interface IEffectableValue
+    {
+        void Reset();
+        void Set();
+
+        object BoxedFinalValue { get; }
+        Type ValueType { get; }
+    }
+
+    public enum ModifierOp
+    {
+        Additive,
+        Multiplicative
+    }
+
+    public enum ModifierLifetime
+    {
+        Permanent,
+        OneCommand
+    }
+
+    [Serializable]
+    public struct ValueModifier
+    {
+        public ModifierOp Op;
+        public double Value;
+        public object Source;
+        public ModifierLifetime Lifetime;
+
+        public static ValueModifier Add(double value, object source = null, ModifierLifetime life = ModifierLifetime.Permanent)
+            => new ValueModifier { Op = ModifierOp.Additive, Value = value, Source = source, Lifetime = life };
+
+        public static ValueModifier Mul(double multiplier, object source = null, ModifierLifetime life = ModifierLifetime.Permanent)
+            => new ValueModifier { Op = ModifierOp.Multiplicative, Value = multiplier, Source = source, Lifetime = life };
+    }
+
+    [Serializable]
+    public abstract class EffectableValue<T> : IEffectableValue where T : struct, IConvertible
+    {
+        public T BaseValue;
+
+        private readonly List<ValueModifier> _modifiers = new();
+        public IReadOnlyList<ValueModifier> Modifiers => _modifiers;
+
+        public virtual T FinalValue
+        {
+            get
+            {
+                double add = 0.0;
+                double mul = 1.0;
+
+                for (int i = 0; i < _modifiers.Count; i++)
+                {
+                    var modifier = _modifiers[i];
+                    if (modifier.Op == ModifierOp.Additive)
+                        add += modifier.Value;
+                    else
+                        mul *= modifier.Value;
+                }
+
+                return FromDouble((ToDouble(BaseValue) + add) * mul);
+            }
+        }
+
+        object IEffectableValue.BoxedFinalValue => FinalValue;
+        Type IEffectableValue.ValueType => typeof(T);
+
+        public void Add(ValueModifier modifier) => _modifiers.Add(modifier);
+
+        public void AddAdditive(double value, object source = null, ModifierLifetime life = ModifierLifetime.Permanent)
+            => _modifiers.Add(ValueModifier.Add(value, source, life));
+
+        public void AddMultiplier(double multiplier, object source = null, ModifierLifetime life = ModifierLifetime.Permanent)
+            => _modifiers.Add(ValueModifier.Mul(multiplier, source, life));
+
+        public void RemoveFrom(object source) => _modifiers.RemoveAll(modifier => Equals(modifier.Source, source));
+
+        public virtual void Reset() => _modifiers.RemoveAll(modifier => modifier.Lifetime == ModifierLifetime.OneCommand);
+
+        public virtual void Set()
+        {
+            for (int i = 0; i < _modifiers.Count; i++)
+            {
+                var modifier = _modifiers[i];
+                if (modifier.Lifetime == ModifierLifetime.OneCommand)
+                {
+                    modifier.Lifetime = ModifierLifetime.Permanent;
+                    _modifiers[i] = modifier;
+                }
+            }
+        }
+
+        public virtual void HardReset() => _modifiers.Clear();
+
+        public virtual void HardSet()
+        {
+            BaseValue = FinalValue;
+            _modifiers.Clear();
+        }
+
+        protected double ToDouble(T value) => Convert.ToDouble(value);
+        protected T FromDouble(double value) => (T)Convert.ChangeType(value, typeof(T));
+    }
+
+    [Serializable] public class EffectableInt : EffectableValue<int> { }
+    [Serializable] public class EffectableFloat : EffectableValue<float> { }
+
+    [Serializable]
+    public abstract class ReplaceableValue<T> : IEffectableValue
+    {
+        public T BaseValue;
+        public bool IsAltered;
+        public T AlteredValue;
+
+        public virtual T FinalValue => IsAltered ? AlteredValue : BaseValue;
+
+        object IEffectableValue.BoxedFinalValue => FinalValue;
+        Type IEffectableValue.ValueType => typeof(T);
+
+        public void Replace(T value)
+        {
+            AlteredValue = value;
+            IsAltered = true;
+        }
+
+        public virtual void Reset()
+        {
+            AlteredValue = default;
+            IsAltered = false;
+        }
+
+        public virtual void Set()
+        {
+            BaseValue = FinalValue;
+            Reset();
+        }
+    }
+
+    [Serializable] public class ReplaceableBool : ReplaceableValue<bool> { }
+    [Serializable] public class ReplaceableInt : ReplaceableValue<int> { }
+    [Serializable] public class ReplaceableFloat : ReplaceableValue<float> { }
 }
-
-/// <summary>
-/// 기본값 + 가산 보정 + 곱연 보정 구조의 수치.
-/// TempAdditive/TempMultiplier는 커맨드 1회 실행 동안만 유효하며,
-/// CommandEvent.Run의 finally에서 ResetContext → Reset으로 자동 정리된다.
-///
-/// FinalValue = (BaseValue + Additive + TempAdditive) × (Multiplier × TempMultiplier)
-/// </summary>
-[Serializable]
-public abstract class EffectableValue<T> : IEffectableValue where T : struct, IConvertible
-{
-    public T BaseValue;
-    public float Multiplier;
-    public float TempMultiplierForOneCommand;
-    public T Additive;
-    public T TempAdditiveForOneCommand;
-
-    public virtual T FinalValue =>
-        FromDouble(
-            (ToDouble(BaseValue) + ToDouble(Additive) + ToDouble(TempAdditiveForOneCommand))
-            * (Multiplier * TempMultiplierForOneCommand)
-        );
-
-    protected abstract float DefaultMultiplier { get; }
-    protected abstract T DefaultAdditive { get; }
-
-    protected double ToDouble(T value) => Convert.ToDouble(value);
-    protected T FromDouble(double value) => (T)Convert.ChangeType(value, typeof(T));
-
-    /// <summary>
-    /// 모든 보정값을 기본값으로 초기화.
-    /// </summary>
-    public virtual void HardReset()
-    {
-        Multiplier = DefaultMultiplier;
-        Additive = DefaultAdditive;
-        TempAdditiveForOneCommand = DefaultAdditive;
-        TempMultiplierForOneCommand = DefaultMultiplier;
-    }
-
-    /// <summary>
-    /// 현재 FinalValue를 BaseValue로 확정하고 보정값 초기화.
-    /// </summary>
-    public virtual void HardSet()
-    {
-        BaseValue = FinalValue;
-        HardReset();
-    }
-
-    /// <summary>
-    /// 임시 보정값만 초기화 (커맨드 1회 실행 후 호출).
-    /// </summary>
-    public virtual void Reset()
-    {
-        TempAdditiveForOneCommand = DefaultAdditive;
-        TempMultiplierForOneCommand = DefaultMultiplier;
-    }
-
-    /// <summary>
-    /// 임시 보정값을 영구 보정값에 반영 후 임시값 초기화.
-    /// </summary>
-    public virtual void Set()
-    {
-        Multiplier *= TempMultiplierForOneCommand;
-        double newAdd = ToDouble(Additive) + ToDouble(TempAdditiveForOneCommand);
-        Additive = FromDouble(newAdd);
-        Reset();
-    }
-}
-
-[Serializable]
-public class EffectableInt : EffectableValue<int>
-{
-    public EffectableInt() => HardReset();
-    protected override float DefaultMultiplier => 1.0f;
-    protected override int DefaultAdditive => 0;
-}
-
-[Serializable]
-public class EffectableFloat : EffectableValue<float>
-{
-    public EffectableFloat() => HardReset();
-    protected override float DefaultMultiplier => 1f;
-    protected override float DefaultAdditive => 0f;
-}
-
-[Serializable]
-public abstract class ReplaceableValue<T> : IEffectableValue
-{
-    public T BaseValue;
-    public bool IsAltered;
-    public T AlteredValue;
-
-    public virtual T FinalValue => IsAltered ? AlteredValue : BaseValue;
-
-    public void Replace(T value)
-    {
-        AlteredValue = value;
-        IsAltered = true;
-    }
-
-    public virtual void Reset()
-    {
-        AlteredValue = default;
-        IsAltered = false;
-    }
-
-    public virtual void Set()
-    {
-        BaseValue = FinalValue;
-        Reset();
-    }
-}
-
-[Serializable] public class ReplaceableBool : ReplaceableValue<bool> { }
-[Serializable] public class ReplaceableInt : ReplaceableValue<int> { }
-[Serializable] public class ReplaceableFloat : ReplaceableValue<float> { }
